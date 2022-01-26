e862d3b69ffb14889d1bdba658eea7b820887268
sunouray <sunraysfs@gmail.com>
2022-01-26 15:41:50 -0800

    Add separate test methods for each test file

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -24,4 +24,28 @@ public class MarkdownParseTest {
         String contents = Files.readString(fileName);
         assertEquals(Arrays.asList(expected), MarkdownParse.getLinks(contents));
     }
+
+    @Test
+    public void getLinks2() throws IOException{
+        String[] expected = { "https://ucsd-cse15l-w22.github.io/week/week3/", "https://www.google.com/" };
+        Path fileName = Path.of("test-file2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(Arrays.asList(expected), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void getLinks3() throws IOException{
+        String[] expected = { "https://ucsd-cse15l-w22.github.io/week/week3/", "sdfgsdfgsdg","sdfsdfsdfsdf"};
+        Path fileName = Path.of("test-file3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(Arrays.asList(expected), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void getLinks4() throws IOException{
+        String[] expected = { "stuff"};
+        Path fileName = Path.of("test-file4.md");
+        String contents = Files.readString(fileName);
+        assertEquals(Arrays.asList(expected), MarkdownParse.getLinks(contents));
+    }
 }
