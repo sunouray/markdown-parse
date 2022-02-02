3ade50653839b0e2dad3cd81134722672eb9ca5e
sunouray <sunraysfs@gmail.com>
2022-02-02 14:54:19 -0800

    Change tester to test original files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -28,7 +28,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks2() throws IOException{
         String[] expected = { "https://ucsd-cse15l-w22.github.io/week/week3/", "https://www.google.com/" };
-        Path fileName = Path.of("test-file2.md");
+        Path fileName = Path.of("test-file2-own.md");
         String contents = Files.readString(fileName);
         assertEquals(Arrays.asList(expected), MarkdownParse.getLinks(contents));
     }
@@ -36,7 +36,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks3() throws IOException{
         String[] expected = { "https://ucsd-cse15l-w22.github.io/week/week3/", "sdfgsdfgsdg","sdfsdfsdfsdf"};
-        Path fileName = Path.of("test-file3.md");
+        Path fileName = Path.of("test-file3-own.md");
         String contents = Files.readString(fileName);
         assertEquals(Arrays.asList(expected), MarkdownParse.getLinks(contents));
     }
@@ -44,7 +44,7 @@ public class MarkdownParseTest {
     @Test
     public void getLinks4() throws IOException{
         String[] expected = { "stuff"};
-        Path fileName = Path.of("test-file4.md");
+        Path fileName = Path.of("test-file4-own.md");
         String contents = Files.readString(fileName);
         assertEquals(Arrays.asList(expected), MarkdownParse.getLinks(contents));
     }
