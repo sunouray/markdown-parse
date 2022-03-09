f5ac83d10fc0051113375d98cefc6fdfffa18cad
sunouray <sunraysfs@gmail.com>
2022-03-09 14:44:42 -0800

    Add CommonMark test

---
+++ TryCommonMark.java
@@ -0,0 +1,34 @@
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+class WordCountVisitor extends AbstractVisitor {
+    int wordCount = 0;
+
+    @Override
+    public void visit(Text text) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        wordCount += text.getLiteral().split("\\W+").length;
+
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(text);
+    }
+}
+
+class TryCommonMark {
+    public static void main(String[] args) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse("This is *Sparta*");
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        System.out.print(renderer.render(document));  // "<p>This is <em>Sparta</em></p>\n"
+
+        Node node = parser.parse("Example\n=======\n\nSome more text");
+        WordCountVisitor visitor = new WordCountVisitor();
+        node.accept(visitor);
+        int x = visitor.wordCount;
+        System.out.println(x);  // 4
+    }
+}
+
